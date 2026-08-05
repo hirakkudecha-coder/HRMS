31cede66093bf2a526789f0d4929490ac5be22fa
Hirak Kudecha <hirakkudecha@gmail.com>
2026-08-05 18:49:05 +0530

    Fix Render DNS resolution for MongoDB Atlas SRV

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,9 @@
 // Import Mongoose to connect to MongoDB database
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Force Node.js to use IPv4 DNS resolution for MongoDB Atlas SRV lookup on Render
+dns.setDefaultResultOrder('ipv4first');
 
 // Function to connect to the MongoDB database
 const connectDB = async () => {
